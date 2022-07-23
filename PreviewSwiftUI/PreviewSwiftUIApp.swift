b2cba34a59d6cfd9c0c5d2149e097613ce724cd9
Ömer Faruk Kılıçaslan <omerrr_2001@hotmail.com>
2022-07-23 12:29:08 +0300

    Initial Commit

---
+++ PreviewSwiftUI/PreviewSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  PreviewSwiftUIApp.swift
+//  PreviewSwiftUI
+//
+//  Created by Ömer Faruk Kılıçaslan on 23.07.2022.
+//
+
+import SwiftUI
+
+@main
+struct PreviewSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
